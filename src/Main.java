6e97ba1bf183c7221b2a9534de6b1b98b9ad9a8f
20133230 <acoustically@kookmin.ac.kr>
2016-12-02 15:08:15 +0900

    make executable file

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
+import java.io.File;
+
 /**
  * Created by acoustically on 16. 11. 29.
  */
 public class Main {
   public static void main(String[] args) {
     try {
-      while(true) {
         BluetoothServer server = new BluetoothServer();
         ReadWriteThread rwTread = server.accept();
         rwTread.start();
-      }
     } catch (Exception e) {
       e.printStackTrace();
     }
